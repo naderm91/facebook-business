4aa990ade8b0fb3f3977bba71f5c1d4f26e3aa23
naderm91 <nader.m@salla.sa>
2023-03-07 09:16:50 +0200

    add canvas json apis

--- pages/auth.php
+++ pages/auth.php
@@ -3,6 +3,5 @@
 $redirect_uri = $_ENV['APP_URL'] . '/?route=access_token';
 
 $url = requestAuth($_ENV['APP_ID'], $redirect_uri);
-
 // Redirect the user to the login dialog
 header('Location: ' . $url);
